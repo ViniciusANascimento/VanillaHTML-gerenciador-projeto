26abbe7236aa49aac9c77d29327f2e3c17f4cb5a
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-07-25 14:29:56 -0300

    feat: add procfile

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
-const express = require("express");
-
-// Create an Express app and listen for incoming requests on port 3000
+const express = require('express');
+const path = require('path');
 const app = express();
-const router = express.Router();
-const port = process.env.PORT || 3000;
 
-// Use middleware to parse incoming requests with JSON and URL-encoded payloads
-app.use(express.json());
-app.use(express.urlencoded());
+const PORT = process.env.PORT || 3000;
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Internal Server Error");
+// Serve os arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para servir o index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Handle GET requests to the root URL
-router.get("/", (req, res) => {
-  res.send("Welcome to the Webhook Server!");
-});
-
-// Handle POST requests to specific URLs i.e. webhook endpoints
-router.post("/webhook-1", (req, res) => {
-  console.log(req.body);
-  res.send("Webhook 1 successfully received.");
-});
-
-router.post("/webhook-2", (req, res) => {
-  console.log(req.body);
-  res.send("Webhook 2 successfully received.");
-});
-
-// Mount the router middleware
-app.use(router);
-
-// Start the server and listen for incoming connections
-app.listen(port, () => {
-  console.log(`Server running at https://localhost:${port}/`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
